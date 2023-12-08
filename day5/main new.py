b0d2285bcd22d834f49570d454fca2a9d7f1a19c
Joachim Winsnes / Intility AS <joachim.winsnes@intility.no>
2023-12-08 10:49:17 +0100

    refactor

--- day5/main new.py
+++ day5/main new.py
@@ -1,4 +1,4 @@
-f = open("test.txt", "r")
+f = open("input.txt", "r")
 
 
 seeds = f.readline().split(":")[1].strip().replace("\n", "").split(" ")
@@ -82,73 +82,93 @@ def createRanges(maps):
         drange = ([(i) for i in range(int(map[0]), int(map[0]) + int(map[2]))]) + drange
     return srange, drange
 
-def calculate(fromtypes, srange, drange):
+def getRangeVal(maps):
+    ss = []
+    se = []
+    ds = []
+    de = []
+    for index, map in enumerate(maps):
+        ss.append(int(map[1]))
+        se.append(int(map[1]) + int(map[2]) - 1)
+        ds.append(int(map[0]))
+        de.append(int(map[0]) + int(map[2]) - 1)
+    return ss, se, ds, de
+
+
+
+def calculate(fromtypes, ss, se, ds, de):
     temp = []
     for fromtype in fromtypes:
-        if not int(fromtype) in srange:
+        found = False
+        for i in range(0, len(ss) ):
+            if int(fromtype) in range(ss[i], se[i] + 1):
+                print("Type is in list")
+                temp.append(int(fromtype) + int(ds[i] - ss[i]))
+                found = True
+        if not found:
             temp.append(int(fromtype))
-        else:
-            for index, source in enumerate(srange):
-                if int(fromtype) == int(source):
-                    temp.append(drange[index])
+    #print(temp)
     return temp
 
 print("soils")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(seedtosoilmap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(seedtosoilmap)
 print("calculating")
-soils = calculate(seeds, sourcerange, destrange)
+soils = calculate(seeds, ss, se, ds, de)
+
 
 print("fert")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(soiltofertilizermap)
-print(sourcerange)
-print(destrange)
-print(soils)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(soiltofertilizermap)
 print("calculating")
-fertelizers = calculate(soils, sourcerange, destrange)
-print(fertelizers)
+fertelizers = calculate(soils, ss, se, ds, de)
+
+
 
 print("water")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(fertilizertowatermap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(fertilizertowatermap)
 print("calculating")
-print(sourcerange)
-print(destrange)
-print(fertelizers)
-waters = calculate(fertelizers, sourcerange, destrange)
-print(waters)
+waters = calculate(fertelizers, ss, se, ds, de)
+
 
 print("light")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(watertolightmap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(watertolightmap)
 print("calculating")
-lights = calculate(waters, sourcerange, destrange)
+lights = calculate(waters, ss, se, ds, de)
 
 print("temp")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(lighttotemperaturemap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(lighttotemperaturemap)
 print("calculating")
-temperatures = calculate(lights, sourcerange, destrange)
+temperatures = calculate(lights, ss, se, ds, de)
 
 print("humi")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(temperaturetohumiditymap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(temperaturetohumiditymap)
 print("calculating")
-humidities = calculate(temperatures, sourcerange, destrange)
+humidities = calculate(temperatures, ss, se, ds, de)
 
 print("location")
 sourcerange = []
 destrange = []
-sourcerange, destrange = createRanges(humiditytolocationmap)
+ss, se, ds, de = [], [], [], []
+ss, se, ds, de = getRangeVal(humiditytolocationmap)
 print("calculating")
-locations = calculate(humidities, sourcerange, destrange)
+locations = calculate(humidities, ss, se, ds, de)
 
 
 
